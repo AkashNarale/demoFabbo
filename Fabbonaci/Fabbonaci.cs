57250fad13e79a38a8505feac79c9a244bdab9bf
AkashNarale <akashnarale43@gmail.com>
2022-10-31 13:02:42 +0530

    Fibbonacci Series

---
+++ Fabbonaci/Fabbonaci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabbonaci
+{
+    class Fabbonaci : Class1
+    {
+        public override void fibbo()
+        {
+            Console.WriteLine("enter the count");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            int n1 = 1, n2 = 2, n3;
+            for (int i = 1; i < count; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(" " + n3);
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
---
+++ Fabbonaci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabbonaci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fabbonaci obj = new Fabbonaci();
+            obj.fibbo();
+            Console.ReadLine();
+        }
+    }
+}
